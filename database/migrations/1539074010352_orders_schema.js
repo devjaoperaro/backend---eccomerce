35493875f98ed54cadcba18fb98244e30a4e9611
Thauã Santos <nero.rbr@gmail.com>
2018-10-09 16:48:55 -0300

    relationships done

--- database/migrations/1539074010352_orders_schema.js
+++ database/migrations/1539074010352_orders_schema.js
@@ -18,6 +18,13 @@ class OrdersSchema extends Schema {
                 .enu('status', ['awaiting', 'cancelled', 'shipped', 'paid'])
                 .defaultTo('awaiting')
 
+            table.integer('cart_id').unsigned()
+            table
+                .foreign('cart_id')
+                .references('id')
+                .inTable('carts')
+                .onDelete('cascade')
+
             table.timestamps()
         })
     }
