90e4ea8f6f0a85c6988f21ec10689b3e038e9515
Thauã Santos <nero.rbr@gmail.com>
2018-11-03 16:34:56 -0300

    orderItem refactoring

--- database/migrations/1539074010352_orders_schema.js
+++ database/migrations/1539074010352_orders_schema.js
@@ -7,23 +7,11 @@ class OrdersSchema extends Schema {
     up() {
         this.create('orders', table => {
             table.increments()
-            table.decimal('subtotal', 12, 2).notNullable()
-            table
-                .decimal('discount', 12, 2)
-                .notNullable()
-                .defaultTo(0.0)
-            table.decimal('total', 12, 2).notNullable()
+            table.decimal('total', 12, 2)
 
             table
-                .enu('status', ['awaiting', 'cancelled', 'shipped', 'paid'])
-                .defaultTo('awaiting')
-
-            table.integer('cart_id').unsigned()
-            table
-                .foreign('cart_id')
-                .references('id')
-                .inTable('carts')
-                .onDelete('cascade')
+                .enu('status', ['pending', 'cancelled', 'shipped', 'paid'])
+                .defaultTo('pending')
 
             table.timestamps()
         })
