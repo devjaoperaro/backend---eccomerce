e04ec40445eefa897941225d514bd9aa919f7bda
Thauan Santos <nero.rbr@gmail.com>
2019-03-06 10:57:31 -0300

    linting

--- database/migrations/1551281495575_coupon_user_schema.js
+++ database/migrations/1551281495575_coupon_user_schema.js
@@ -4,29 +4,29 @@
 const Schema = use('Schema')
 
 class CouponUserSchema extends Schema {
-    up() {
-        this.create('coupon_user', table => {
-            table.increments()
-            table.integer('coupon_id').unsigned()
-            table.integer('user_id').unsigned()
-            table.timestamps()
+  up() {
+    this.create('coupon_user', table => {
+      table.increments()
+      table.integer('coupon_id').unsigned()
+      table.integer('user_id').unsigned()
+      table.timestamps()
 
-            table
-                .foreign('coupon_id')
-                .references('id')
-                .inTable('coupons')
-                .onDelete('cascade')
-            table
-                .foreign('user_id')
-                .references('id')
-                .inTable('users')
-                .onDelete('cascade')
-        })
-    }
+      table
+        .foreign('coupon_id')
+        .references('id')
+        .inTable('coupons')
+        .onDelete('cascade')
+      table
+        .foreign('user_id')
+        .references('id')
+        .inTable('users')
+        .onDelete('cascade')
+    })
+  }
 
-    down() {
-        this.drop('coupon_users')
-    }
+  down() {
+    this.drop('coupon_user')
+  }
 }
 
 module.exports = CouponUserSchema
--- database/migrations/1551281978901_coupon_product_schema.js
+++ database/migrations/1551281978901_coupon_product_schema.js
@@ -4,29 +4,29 @@
 const Schema = use('Schema')
 
 class CouponProductSchema extends Schema {
-    up() {
-        this.create('coupon_product', table => {
-            table.increments()
-            table.integer('coupon_id').unsigned()
-            table.integer('product_id').unsigned()
-            table.timestamps()
+  up() {
+    this.create('coupon_product', table => {
+      table.increments()
+      table.integer('coupon_id').unsigned()
+      table.integer('product_id').unsigned()
+      table.timestamps()
 
-            table
-                .foreign('coupon_id')
-                .references('id')
-                .inTable('coupons')
-                .onDelete('cascade')
-            table
-                .foreign('product_id')
-                .references('id')
-                .inTable('products')
-                .onDelete('cascade')
-        })
-    }
+      table
+        .foreign('coupon_id')
+        .references('id')
+        .inTable('coupons')
+        .onDelete('cascade')
+      table
+        .foreign('product_id')
+        .references('id')
+        .inTable('products')
+        .onDelete('cascade')
+    })
+  }
 
-    down() {
-        this.drop('coupon_products')
-    }
+  down() {
+    this.drop('coupon_product')
+  }
 }
 
 module.exports = CouponProductSchema
