2a8c11670be038127ff33f00fd0a2988cc3a9406
Thauan Santos <nero.rbr@gmail.com>
2019-03-08 07:03:36 -0300

    separação das rotas por arquivos menores

--- start/routes/admin.js
+++ start/routes/admin.js
@@ -1,55 +1,7 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.0/routing
-|
-*/
-
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
-
-/**
- * Current User
- */
-Route.get('v1/me', 'UserController.me')
-  .as('me')
-  .middleware('auth')
-
-/**
- * Auth Routes used for admins and users
- */
-Route.group(() => {
-  Route.post('/register', 'AuthController.register')
-    .as('auth.register')
-    .validator('Clients/ClientRegister')
-
-  Route.post('login', 'AuthController.login')
-    .as('auth.login')
-    .validator('Clients/ClientLogin')
-
-  Route.post('refresh', 'AuthController.refresh')
-    .as('auth.refresh')
-    .validator('Clients/ClientRefreshToken')
-
-  Route.post('logout', 'AuthController.logout')
-    .as('auth.logout')
-    .middleware(['auth'])
-
-  Route.get('reset-password', 'AuthController.remember').as('auth.remember')
-  Route.post('reset-password', 'AuthController.forgot').as('auth.forgot')
-  Route.put('reset-password', 'AuthController.reset').as('auth.reset')
-  // .validator('Clients/ClientRefreshToken')
-})
-  .prefix('v1/auth')
-  .namespace('Auth')
 
 /**
  * Administration Routes V1
