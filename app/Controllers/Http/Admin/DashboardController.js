fb64d8b36db2881e311707aa326e61311b5d9b23
Thauan Santos <nero.rbr@gmail.com>
2019-03-08 08:06:09 -0300

    eslint identation fix

--- app/Controllers/Http/Admin/DashboardController.js
+++ app/Controllers/Http/Admin/DashboardController.js
@@ -2,14 +2,14 @@
 const DB = use('Database')
 
 class DashboardController {
-    async index({ response }) {
-        const users = await DB.from('users').getCount()
-        const orders = await DB.from('orders').getCount()
-        const products = await DB.from('products').getCount()
-        const revenues = await DB.from('orders').getSum('total')
+  async index({ response }) {
+    const users = await DB.from('users').getCount()
+    const orders = await DB.from('orders').getCount()
+    const products = await DB.from('products').getCount()
+    const revenues = await DB.from('orders').getSum('total')
 
-        return response.send({ users, revenues, orders, products })
-    }
+    return response.send({ users, revenues, orders, products })
+  }
 }
 
 module.exports = DashboardController
