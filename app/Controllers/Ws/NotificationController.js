0fa90a76e37d896b42a1b1727db251d47216cc3d
Thauan Santos <nero.rbr@gmail.com>
2019-03-15 01:56:29 -0300

    base dos websockets functionando

--- app/Controllers/Ws/NotificationController.js
+++ app/Controllers/Ws/NotificationController.js
@@ -1,9 +1,29 @@
 'use strict'
 
 class NotificationController {
-  constructor ({ socket, request }) {
+  constructor({ socket, request, auth }) {
     this.socket = socket
     this.request = request
+    this.loadUser(auth)
+      .then(user => {
+        socket.broadcast('new:connection', {
+          message: 'Nova Conexão!',
+          user
+        })
+      })
+      .catch(e => {
+        throw e
+      })
+  }
+
+  async loadUser(auth) {
+    const user = await auth.getUser()
+    return user
+  }
+
+  onMessage(message) {
+    console.log('Nova mensagem', message)
+    this.socket.broadcastToAll('message', message)
   }
 }
 
--- start/socket.js
+++ start/socket.js
@@ -15,4 +15,4 @@
 
 const Ws = use('Ws')
 
-Ws.channel('notifications', 'NotificationController').middleware(['auth'])
+Ws.channel('notifications', 'NotificationController') // .middleware(['auth'])
