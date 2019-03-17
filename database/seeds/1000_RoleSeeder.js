a36d68f97070f55dde59a4cbacca3b6d8bac2a86
Thauan Santos <nero.rbr@gmail.com>
2019-03-17 08:12:29 -0300

    melhorias de qualidade

--- database/seeds/1000_RoleSeeder.js
+++ database/seeds/1000_RoleSeeder.js
@@ -11,29 +11,28 @@
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
-const Factory = use('Factory')
 const Role = use('Role')
 
 class RoleSeeder {
-    async run() {
-        const admin = new Role()
-        admin.name = 'Admin'
-        admin.slug = 'admin'
-        admin.description = 'Administrador do Sistema'
-        await admin.save()
+  async run() {
+    const admin = new Role()
+    admin.name = 'Admin'
+    admin.slug = 'admin'
+    admin.description = 'Administrador do Sistema'
+    await admin.save()
 
-        const manager = new Role()
-        manager.name = 'Gerente'
-        manager.slug = 'manager'
-        manager.description = 'Manager do Sistema'
-        await manager.save()
+    const manager = new Role()
+    manager.name = 'Gerente'
+    manager.slug = 'manager'
+    manager.description = 'Manager do Sistema'
+    await manager.save()
 
-        const client = new Role()
-        client.name = 'Cliente'
-        client.slug = 'client'
-        client.description = 'Cliente do Sistema'
-        await client.save()
-    }
+    const client = new Role()
+    client.name = 'Cliente'
+    client.slug = 'client'
+    client.description = 'Cliente do Sistema'
+    await client.save()
+  }
 }
 
 module.exports = RoleSeeder
